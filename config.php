5dda4e2405da2ee6c366760cbdb38dac3329edbd
Newbilius <newbilius@gmail.com>
2013-02-24 21:37:25 +0600

    fix

--- config.php
+++ config.php
@@ -1,4 +1,13 @@
 <?
+/**
+ * Скрипт сохранения избранного пользователя ХабраХабра.
+ *
+ * Лицензия - делайте с файлом что хотите, только не кидайтесь помидорами и автора из файлов не выкидывайте.
+ *
+ * @author     Моисеев Дмитрий aka Newbilius (Nubilius) <newbilius@gmail.com>
+ * @copyright  2013
+ * @link       https://github.com/Newbilius/habr_to_fb2
+ */
 
 header('Content-Type: text/html; charset=utf-8');
 include_once("habrahabr_parser/holy_habr_api.php");
